143060e8a237e55bdc0b9513240a1914478d03f5
Akanksha <joshiaj33@gmail.com>
2025-01-01 18:00:02 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
 # To run the scene, use the following command in your terminal:
 # python -m manim introduction.py[FILENAME.py]
-
-
-# 1. Introduction : 20 seconds
-# 2. Vector: 30 + 20 + 20 (operations) + 30(addition) + 30(subtraction) + 25(scalar) + 30(dot) + 30(cross) seconds
-# 3. Vector spaces: 20 (introduction) + 20 (properties) + 40 (subspace) + 20 (linear combination and span)
-#
-# Matrix: 70(introduction) + 45(inverses) + 10 (visualization)
-#
-# checkpoint : 480 seconds
-# Linear Independence: 46 (Introduction) +  42 (Basis)
-# checkpoint: 568 seconds
-# Linear Transformation: 6 (Introduction) + 9 (Properties) + 10 (Animation)
-# checkpoint: 593 seconds
-# Determinants: 40 (Introduction + Definition + Geometric Interpertation)
-
-# IN progress
-# Checkpoint: 633 seconds 
-# Goal: 600 seconds (10 minutes)
